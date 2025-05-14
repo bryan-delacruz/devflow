de39893779317e5ab15752fab997b9047b8c444d
Bryan De La Cruz <bdelacruz@infracommerce.lat>
2025-05-14 11:03:38 -0500

    Implement ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
       <h1 className="text-3xl text-slate-300">Home</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
